e0cd21e7cf5fca1de2bd3d174b18fb60ade5e88c
satishsethi24-spec <satishsethi24@gmail.com>
2026-01-11 17:08:46 +0530

    Add files via upload

---
+++ Birthdays.py
@@ -0,0 +1,12 @@
+print("Hello do you want to know the birthday dates of me and the people I know? " \
+"If yes then stay tuned" ) 
+Me= "28-11-2015"
+Dad= "23-11-1983"
+Mom= "19-10-1987"
+Sumeera= "20-12-2014"
+Kenisha= "8-10-2015"
+print(Me)
+print(Dad)
+print(Mom)
+print(Sumeera)
+print(Kenisha)
