596b9d909e11201dd2908e60ef434f0e215b2497
satishsethi24-spec <satishsethi24@gmail.com>
2026-02-08 13:01:21 +0530

    Add files via upload

---
+++ After class projects/Oddeven.py
@@ -0,0 +1,12 @@
+
+# To find the number is odd or even
+
+num = int(input("Enter the number : "))
+
+if num % 2 == 0:
+
+print("The number is even")
+
+else:
+
+print("The number is odd")
---
+++ After class projects/Squareroot.py
@@ -0,0 +1,6 @@
+
+import math
+
+number = 50
+
+print(math.sqrt(number))
